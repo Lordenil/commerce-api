4ad88e47be2b7488af30e1999258f40de088b71e
dalvarez <dalvarez@tsgroup.com.co>
2025-10-29 23:10:29 -0500

    ADD: product controller and use-case

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './infraestructure/db/entities/typeorm.config';
+import { ProductModule } from './application/modules/product.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot(typeOrmConfig),
+    ProductModule,
+  ],
+})
+export class AppModule {}
